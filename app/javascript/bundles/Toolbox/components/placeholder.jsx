9d4cf0652c2ed47bfee4ee798bc9ab2258669d53
Thomas Wilson <twilson.spqr@gmail.com>
2018-03-26 20:37:59 -0400

    Icons adjusted

--- app/javascript/bundles/Toolbox/components/placeholder.jsx
+++ app/javascript/bundles/Toolbox/components/placeholder.jsx
@@ -5,23 +5,23 @@
          <div class="ct" id="t5">
           <ul id="menu">
             <a href="#t1"><li class="icon fas fa-calculator" id="uno"></li></a>
-            <a href="#t2"><li class="icon fas fa-gavel" id="dos"></li></a>
-            <a href="#t3"><li class="icon fas fa-clock" id="tres"></li></a>
-            <a href="#t4"><li class="icon fas fa-magic" id="cuatro"></li></a>
+            <a href="#t2"><li class="icon fas fa-clock" id="dos"></li></a>
+            <a href="#t3"><li class="icon fas fa-magic" id="tres"></li></a>
+            <a href="#t4"><li class="icon fas fa-gavel" id="cuatro"></li></a>
             <a href="#t5"><li class="icon fa fa-plus-circle" id="cinco"></li></a>
           </ul>
           <div class="page" id="p1">
-             <section class="icon fa fa-bolt"><span class="title">Bolt</span><span class="hint">Like this pen to see the magic!...<br> Just kidding, it won't happen anything but I'll be really happy If you do so.</span></section>
+             <section class="icon fas fa-calculator"><span class="title">Stat Calculator</span><span class="hint">Like this pen to see the magic!...<br> Just kidding, it won't happen anything but I'll be really happy If you do so.</span></section>
           </div>
           <div class="page" id="p2">
-            <section class="icon fa fa-keyboard-o"><span class="title">Type</span></section>
+            <section class="icon fas fa-clock"><span class="title">Initiative Tracker</span></section>
           </div>
           <div class="page" id="p3">
-            <section class="icon fa fa-rocket"><span class="title">Rocket</span></section>
+            <section class="icon fas fa-magic"><span class="title">Spell Tracker</span></section>
           </div>
           <div class="page" id="p4">
-            <section class="icon fa fa-dribbble">
-              <span class="title">Dribbble</span>
+            <section class="icon fas fa-gavel">
+              <span class="title">Sunder Calculator</span>
               <p class="hint">
                 <a href="https://dribbble.com/albertohartzet" target="_blank">Im ready to play, <span class="hint line-trough">invite me </span> find me</a>
               </p>
